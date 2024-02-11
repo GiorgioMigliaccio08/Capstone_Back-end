154e768c36a1ea1a5649e05cf30962379016ad6a
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2024-02-11 20:12:52 +0100

    Add third class: Archive

--- src/main/java/giorgiomigliaccio/Capstone_Backend/entities/Archive.java
+++ src/main/java/giorgiomigliaccio/Capstone_Backend/entities/Archive.java
@@ -1,4 +1,46 @@
 package giorgiomigliaccio.Capstone_Backend.entities;
 
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name= "archivio")
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
 public class Archive {
+    @Id
+    @GeneratedValue
+    private UUID id;
+    private String luogoVisita;
+    private LocalDate dataVisitaEffettuata;
+    private LocalDate dataVisitaControllo;
+    private String tipoVisitaEffettuata;
+
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Archive(String luogoVisita, LocalDate dataVisitaEffettuata, LocalDate dataVisitaControllo,String tipoVisitaEffettuata ) {
+        this.luogoVisita = luogoVisita;
+        this.dataVisitaEffettuata = dataVisitaEffettuata;
+        this.dataVisitaControllo = dataVisitaControllo;
+        this.tipoVisitaEffettuata = tipoVisitaEffettuata;
+
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+        if (user != null) {
+            user.getDocumenti().add(this);
+        }
+    }
 }
