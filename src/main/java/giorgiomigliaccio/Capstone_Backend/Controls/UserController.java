48f8065924474bf44a3c65219596a25db9113f30
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2024-02-12 16:18:36 +0100

    Add User Controller and User Service

---
+++ src/main/java/giorgiomigliaccio/Capstone_Backend/Controls/UserController.java
@@ -0,0 +1,71 @@
+package giorgiomigliaccio.Capstone_Backend.Controls;
+
+
+import giorgiomigliaccio.Capstone_Backend.Exceptions.NotFoundException;
+import giorgiomigliaccio.Capstone_Backend.Payloads.UserRegistrationPayload;
+import giorgiomigliaccio.Capstone_Backend.Repositories.UserRepository;
+import giorgiomigliaccio.Capstone_Backend.Services.BookingServices;
+import giorgiomigliaccio.Capstone_Backend.Services.UserServices;
+import giorgiomigliaccio.Capstone_Backend.entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserServices userServices;
+
+    @Autowired
+    BookingServices bookingServices;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @PostMapping("")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @ResponseStatus(HttpStatus.CREATED)
+    public User saveUser(@RequestBody @Validated UserRegistrationPayload body) {
+        return userServices.createUser(body);
+    }
+
+    @GetMapping("")
+    @PreAuthorize("hasAnyAuthority('ADMIN','USER')")
+    public Page<User> getUsers(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size,
+                               @RequestParam(defaultValue = "idUser") String sortBy) {
+        return userServices.findAllUsers(page, size, sortBy);
+    }
+    @PreAuthorize("hasAnyAuthority('ADMIN','USER')")
+    @GetMapping("/{userId}")
+    public User getUserById(@PathVariable UUID userId) throws NotFoundException {
+        return userServices.findUserById(userId);
+    }
+    @PreAuthorize("hasAnyAuthority('ADMIN','USER')")
+    @PutMapping("/{userId}")
+    public User updateUser(@PathVariable UUID userId, @RequestBody UserRegistrationPayload body)
+            throws NotFoundException {
+        return userServices.findUserByIdAndUpdate(userId, body);
+    }
+    @DeleteMapping("/{userId}")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteUser(@PathVariable UUID userId) throws NotFoundException {
+        userServices.findUserByIdAndDelete(userId);
+    }
+
+    @GetMapping("/me")
+    @PreAuthorize("hasAnyAuthority('USER', 'ADMIN')")
+    public User getCurrentUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        return userServices.findUserByUsername(username);
+    }
+}
---
+++ src/main/java/giorgiomigliaccio/Capstone_Backend/Services/UserServices.java
@@ -0,0 +1,97 @@
+package giorgiomigliaccio.Capstone_Backend.Services;
+
+import giorgiomigliaccio.Capstone_Backend.Exceptions.BadRequestException;
+import giorgiomigliaccio.Capstone_Backend.Exceptions.NotFoundException;
+import giorgiomigliaccio.Capstone_Backend.Payloads.UserRegistrationPayload;
+import giorgiomigliaccio.Capstone_Backend.Repositories.UserRepository;
+import giorgiomigliaccio.Capstone_Backend.entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import java.util.UUID;
+
+@Service
+public class UserServices {
+    @Autowired
+    private PasswordEncoder bCrypt;
+    @Autowired
+    private UserRepository userRepository;
+
+
+    public Page<User> findAllUsers(int page, int size, String sortBy) {
+        if (size < 0)
+            size = 10;
+        if (size > 100)
+            size = 20;
+
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sortBy));
+        return userRepository.findAll(pageable);
+    }
+
+    public User createUser(UserRegistrationPayload payload) {
+        String email = payload.getEmail();
+
+        if (!isValidEmail(email)) {
+            throw new BadRequestException("L'email inserita non è valida.");
+        }
+
+        userRepository.findByEmail(email).ifPresent(user -> {
+            throw new NotFoundException("Utente non trovato con ID: " + user.getUserId());
+        });
+
+        User newUser = new User( payload.getNome(),payload.getCognome(), payload.getUsername(), payload.getEmail(), payload.getPassword() );
+        return userRepository.save(newUser);
+    }
+
+    private boolean isValidEmail(String email) {
+        return email.matches("[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}");
+    }
+
+    public User findUserById(UUID id) throws NotFoundException {
+        return userRepository.findById(id).orElseThrow(() -> new NotFoundException("Utente non trovato con ID: " + id));
+    }
+
+    public User findUserByIdAndUpdate(UUID id, UserRegistrationPayload payload) throws NotFoundException {
+        User foundUser = findUserById(id);
+        foundUser.setUserId(id);
+        foundUser.setNome(payload.getNome());
+        foundUser.setCognome(payload.getCognome());
+        foundUser.setUsername(payload.getUsername());
+        foundUser.setEmail(payload.getEmail());
+        foundUser.setPassword(bCrypt.encode(payload.getPassword()));
+
+
+        return userRepository.save(foundUser);
+    }
+
+    public void findUserByIdAndDelete(UUID id) throws NotFoundException {
+        User foundUser = findUserById(id);
+        userRepository.delete(foundUser);
+    }
+
+    public User findUserByEmail(String email) throws NotFoundException {
+        return userRepository.findByEmail(email).orElseThrow(() -> new NotFoundException("Utente non trovato con email: " + email));
+    }
+
+    public User findUserByUsername(String username) throws NotFoundException {
+        return userRepository.findByUsername(username).orElseThrow(() -> new NotFoundException("Utente non trovato con username: " + username));
+    }
+
+    public void deleteAllUsers() {
+        userRepository.deleteAll();
+    }
+
+    public User getCurrentUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new NotFoundException("Utente corrente non trovato"));
+    }
+}
