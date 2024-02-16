13f08fbf8e14fd0031130c726828370f03dadb46
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2024-02-16 18:51:40 +0100

    Add new modify

--- src/main/java/giorgiomigliaccio/Capstone_Backend/Security/CorsFilter.java
+++ src/main/java/giorgiomigliaccio/Capstone_Backend/Security/CorsFilter.java
@@ -12,10 +12,15 @@ public class CorsFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");        response.setHeader("Access-Control-Allow-Methods", "*");
-        response.setHeader("Access-Control-Allow-Headers", "*");        response.setHeader("Access-Control-Allow-Max-Age", "600");
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+        response.setHeader("Access-Control-Allow-Methods", "*");
+        response.setHeader("Access-Control-Allow-Headers", "*");
+        response.setHeader("Access-Control-Allow-Max-Age", "600");
+
         if (request.getMethod().equalsIgnoreCase("OPTIONS")) {
-            response.setStatus(HttpServletResponse.SC_OK);        } else {
-            filterChain.doFilter(request, response);        }
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            filterChain.doFilter(request, response);
+        }
     }
 }
