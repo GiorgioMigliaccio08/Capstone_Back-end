82c0feff34876436b142f3ca41ef5df2c4b6e985
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2024-02-22 18:34:46 +0100

    Add new modify to UserRepository

--- src/main/java/giorgiomigliaccio/Capstone_Backend/Repositories/UserRepository.java
+++ src/main/java/giorgiomigliaccio/Capstone_Backend/Repositories/UserRepository.java
@@ -1,9 +1,15 @@
 package giorgiomigliaccio.Capstone_Backend.Repositories;
 
 
+import giorgiomigliaccio.Capstone_Backend.entities.Archive;
+import giorgiomigliaccio.Capstone_Backend.entities.Booking;
 import giorgiomigliaccio.Capstone_Backend.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -11,4 +17,10 @@ import java.util.UUID;
 public interface UserRepository extends JpaRepository <User, UUID>{
     Optional <User> findByEmail (String email);
     Optional <User> findByUsername (String username);
+
+    @Query("SELECT b FROM Booking b WHERE b.user= :user")
+    List<Booking> getAllBookings(@Param("user") User user);
+
+    @Query("SELECT b FROM Archive b WHERE b.user= :user")
+    List<Archive> getAllDocuments(@Param("user") User user);
 }
